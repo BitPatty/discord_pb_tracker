5128156badde817c9ed76ae05504aa426a687f25
Matteias Collet <matteias.collet@bluewin.ch>
2020-01-01 23:17:30 +0100

    add preview image to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,21 @@
         <h1 class="title is-1">
             Discord PB Tracker
         </h1>
+        <h2 class="subtitle is-3">A
+            <a href="https://speedrun.com" title="Speedrun.com" target="_blank" rel="noreferrer">Speedrun.com</a>
+            PB Tracker for Discord.</h2>
         @auth
             <a class="button is-large is-primary" href="{{ route('dashboard') }}">Dashboard</a>
         @else
-            <a class="button is-large is-primary" href="{{ route('login') }}">Login</a>
-        @endauth
+            <div class="section">
+                <a class="button is-large is-primary" href="{{ route('login') }}">Login with Discord</a>
+            </div>
+            <div>
+                <img alt="Message preview" src="/img/preview.png"/>
+            </div>
     </div>
+    @endauth
+</div>
 </div>
 </body>
 </html>
