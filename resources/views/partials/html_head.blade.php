289bf21d8e19bf163cc9d1788993329b029b2c19
Matteias Collet <matteias.collet@bluewin.ch>
2020-03-03 22:24:26 +0100

    Update html_head.blade.php

--- resources/views/partials/html_head.blade.php
+++ resources/views/partials/html_head.blade.php
@@ -10,4 +10,5 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet"/>
     <link href="/style.css" rel="stylesheet">
     <title>{{ $title }}</title>
+    <script>var _paq=window._paq||[];_paq.push(["disableCookies"]),_paq.push(["trackPageView"]),_paq.push(["enableLinkTracking"]),function(){var e="//dna.zint.ch/";_paq.push(["setTrackerUrl",e+"pdna"]),_paq.push(["setSiteId","6"]);var a=document,t=a.createElement("script"),p=a.getElementsByTagName("script")[0];t.type="text/javascript",t.async=!0,t.defer=!0,t.src=e+"jdna",p.parentNode.insertBefore(t,p)}();</script>
 </head>
