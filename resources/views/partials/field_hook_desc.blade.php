950268c6cb7da4fa49014c50159623ea61a96ed2
Matteias Collet <matteias.collet@bluewin.ch>
2020-06-29 00:05:24 +0200

    minor style changes

--- resources/views/partials/field_hook_desc.blade.php
+++ resources/views/partials/field_hook_desc.blade.php
@@ -1,8 +1,7 @@
 <div class="field ">
     <label class="label">Description</label>
     <div class="control">
-        <textarea id="frm_desc" name="frm_desc" class="textarea" placeholder="PB Tracker for my server"
-                  maxlength="2048">@isset($value) {{ $value }} @endisset</textarea>
+        <textarea id="frm_desc" name="frm_desc" class="textarea" placeholder="PB Tracker for my server" rows=2 maxlength="2048">@isset($value) {{ $value }} @endisset</textarea>
     </div>
     <p class="help">The webhook description (optional).</p>
 </div>
